9b7bc2745f5dc040c582e0a4bddb86650346ccc4
ITPRESCO\kk.kuo <kk.kuo@presco.ws>
2024-08-05 18:14:34 +0800

    side project finished

---
+++ app.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" button below</p>
+    <div class="options-container">
+      <Option
+        v-for="option in optionsArray"
+        :key="option.title"
+        :option="option"
+        :options="options"
+      />
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    </div>
+    <div class="cards-container">
+      <!-- <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p>x</p>
+      </div> -->
+      <CardName
+        v-for="name in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="() => removeName(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Gender, Popularity, Length, names } from '@/data';
+
+  interface OptionsSate {
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  }
+
+  const options = reactive<OptionsSate>({
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  });
+
+  const selectedNames = ref<string[]>([]);
+
+  const computeSelectedNames = () => {
+    const filteredNames = names
+      .filter((name) => name.gender === options.gender)
+      .filter((name) => name.popularity === options.popularity)
+      .filter((name) => {
+        if (options.length === Length.ALL) return true;
+        else return name.length === options.length;
+      });
+
+    selectedNames.value = filteredNames.map((name) => name.name);
+  };
+
+  const removeName = (index: number) => {
+    const filteredNames = [...selectedNames.value];
+    filteredNames.splice(index, 1);
+    selectedNames.value = filteredNames;
+  };
+
+  const optionsArray = [
+    {
+      title: '1) Choose a gender',
+      category: 'gender',
+      buttons: [Gender.BOY, Gender.UNISEX, Gender.GIRL],
+    },
+    {
+      title: "2) Choose the name's popularity",
+      category: 'popularity',
+      buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+    },
+    {
+      title: "3) Choose the name's length",
+      category: 'length',
+      buttons: [Length.SHORT, Length.LONG, Length.ALL],
+    },
+  ];
+</script>
+
+<style scoped>
+  .container {
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgb(27, 60, 138);
+    max-width: 50rem;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  .options-container {
+    background-color: rgb(255, 238, 236);
+    border-radius: 2rem;
+    padding: 1rem;
+    width: 95%;
+    margin: 0 auto;
+    margin-top: 4rem;
+    position: relative;
+  }
+
+  .primary {
+    background-color: rgb(249, 87, 89);
+    color: white;
+    border: none;
+    padding: 0.75rem 4rem;
+    font-size: 1rem;
+    cursor: pointer;
+    font-weight: 200;
+    border-radius: 6.5rem;
+    margin-top: 1rem;
+  }
+
+  .cards-container {
+    display: flex;
+    margin-top: 3rem;
+    flex-wrap: wrap;
+  }
+</style>
